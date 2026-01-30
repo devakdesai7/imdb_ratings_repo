b084f089aac8028eead4f5e2b58c164660f78d02
devakdesai7 <216946260+devakdesai7@users.noreply.github.com>
2026-01-31 02:25:42 +0530

    Refactor the basic styling as a function that will be applied to all plotly figures on the app. Plot the distribution of number of votes given to movies.

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.express as px
+
+def apply_basic_style_to_figure(fig):
+    fig.update_traces(
+        marker_line_color = 'black',
+        marker_line_width = 1,
+        marker_color = '#FF2F00',
+        opacity = 0.7 
+    )
+
+
+    fig.update_layout(
+        shapes = [
+            dict(
+                type="rect",
+                xref="x domain",
+                yref="y domain",
+                x0=0,
+                y0=0,
+                x1=1,
+                y1=1,
+                line=dict(color="white", width=2)
+            )
+        ]
+    )
+
+    return fig
 
 st.markdown(
     "<h1 style='color:rgb(255, 47, 0); text-align:center; font-family:'Times New Roman';'>IMDb Ratings analysis for Movies that are publicly available</h1>",
@@ -23,31 +50,30 @@ fig = px.histogram(
     },
     nbins = ((int(max_ratings - min_ratings)) * 2),
     title = 'Ratings distribution in the selected range of ratings'
-)
+)   
 
-fig.update_traces(
-    marker_line_color = 'black',
-    marker_line_width = 1,
-    marker_color = '#FF2F00',
-    opacity = 0.7 
-)
-
-fig.update_layout(
-    shapes = [
-        dict(
-            type="rect",
-            xref="x domain",
-            yref="y domain",
-            x0=0,
-            y0=0,
-            x1=1,
-            y1=1,
-            line=dict(color="white", width=2)
-        )
-    ]
-)
+fig.update_yaxes(title_text="Number of Movies")
+apply_basic_style_to_figure(fig)
 
 st.plotly_chart(fig)
+
+data['log_scaled_numVotes'] = np.log10(data['numVotes'])
+
+with st.form("my_form"):
+    min_votes = st.number_input("How much votes should a movie have atleast to be included in the distribution?: ", min_value=1, max_value=1600000, value=1)
+    max_votes = st.number_input("How much votes should a movie have atmost to be included in the distribution?: ", min_value=1, max_value=1600000, value=1000000)
+    submitted = st.form_submit_button("Submit Values")
+
+num_votes_histogram = px.histogram(
+    data,
+    x=data['log_scaled_numVotes'],
+    nbins = int(np.log10(max_votes)) - int(np.log10(min_votes)),
+    title="Distribution of number of votes given to movies"
+)
+
+apply_basic_style_to_figure(num_votes_histogram)
+
+st.plotly_chart(num_votes_histogram)
 
 
 
