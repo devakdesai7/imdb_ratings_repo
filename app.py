8a18be221e9b22deaf5836de1b6450b65c73a455
devakdesai7 <216946260+devakdesai7@users.noreply.github.com>
2026-01-31 02:53:41 +0530

    Add a sidebar to the app so as to show specifically what the user wants, so now the plots and graphs plotted in the previous commits are shown only if the user chooses to see Analytics

--- app.py
+++ app.py
@@ -29,53 +29,62 @@ def apply_basic_style_to_figure(fig):
 
     return fig
 
-st.markdown(
+page = st.sidebar.selectbox(
+    "☰ Navigation",
+    ['Home', 'Analytics', 'About']
+)
+
+if page == 'Analytics':
+    st.markdown(
     "<h1 style='color:rgb(255, 47, 0); text-align:center; font-family:'Times New Roman';'>IMDb Ratings analysis for Movies that are publicly available</h1>",
     unsafe_allow_html=True
-)
+    )
 
-data = pd.read_csv('dataset/imdb_ratings.csv')
+    data = pd.read_csv('dataset/imdb_ratings.csv')
 
-st.dataframe(data)
+    st.dataframe(data)
 
-min_ratings, max_ratings = st.slider("Select ratings' range", 0, 10, (0, 5))
-filtered = data[(data['averageRating'] >= min_ratings) & (data['averageRating'] <= max_ratings)]
+    min_ratings, max_ratings = st.slider("Select ratings' range", 0, 10, (0, 5))
+    filtered = data[(data['averageRating'] >= min_ratings) & (data['averageRating'] <= max_ratings)]
 
-fig = px.histogram(
-    filtered, 
-    x = "averageRating",
-    labels = {
-        'averageRating' : 'Average Rating',
-        'count' : 'Number of Movies'
-    },
-    nbins = ((int(max_ratings - min_ratings)) * 2),
-    title = 'Ratings distribution in the selected range of ratings'
-)   
+    fig = px.histogram(
+        filtered, 
+        x = "averageRating",
+        labels = {
+            'averageRating' : 'Average Rating',
+            'count' : 'Number of Movies'
+        },
+        nbins = ((int(max_ratings - min_ratings)) * 2),
+        title = 'Ratings distribution in the selected range of ratings'
+    )   
 
-fig.update_yaxes(title_text="Number of Movies")
-apply_basic_style_to_figure(fig)
+    fig.update_yaxes(title_text="Number of Movies")
+    apply_basic_style_to_figure(fig)
 
-st.plotly_chart(fig)
+    st.plotly_chart(fig)
 
 
-with st.form("my_form"):
-    min_votes = st.number_input("How much votes should a movie have atleast to be included in the distribution?: ", min_value=1, max_value=1600000, value=1)
-    max_votes = st.number_input("How much votes should a movie have atmost to be included in the distribution?: ", min_value=1, max_value=1600000, value=1000000)
-    submitted = st.form_submit_button("Submit Values")
+    with st.form("my_form"):
+        min_votes = st.number_input("How much votes should a movie have atleast to be included in the distribution?: ", min_value=1, max_value=1600000, value=1)
+        max_votes = st.number_input("How much votes should a movie have atmost to be included in the distribution?: ", min_value=1, max_value=1600000, value=1000000)
+        submitted = st.form_submit_button("Submit Values")
 
-required_df = data[(data['numVotes'] >= min_votes) & (data['numVotes'] <= max_votes)]
-required_df['log_scaled_numVotes'] = np.log10(required_df['numVotes'])
+    required_df = data[(data['numVotes'] >= min_votes) & (data['numVotes'] <= max_votes)]
+    required_df['log_scaled_numVotes'] = np.log10(required_df['numVotes'])
 
-num_votes_histogram = px.histogram(
-    required_df,
-    x=required_df['log_scaled_numVotes'],
-    nbins = int(np.log10(max_votes)) - int(np.log10(min_votes)),
-    title="Distribution of number of votes given to movies"
-)
+    num_votes_histogram = px.histogram(
+        required_df,
+        x=required_df['log_scaled_numVotes'],
+        nbins = int(np.log10(max_votes)) - int(np.log10(min_votes)),
+        title="Distribution of number of votes given to movies"
+    )
 
-apply_basic_style_to_figure(num_votes_histogram)
+    apply_basic_style_to_figure(num_votes_histogram)
 
-st.plotly_chart(num_votes_histogram)
+    st.plotly_chart(num_votes_histogram)
+
+
+
 
 
 
